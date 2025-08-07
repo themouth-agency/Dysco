f6ef745fdb1b3a0b742b0f03fff77c6b0eae451c
MilanWR <milan@8bees.fr>
2025-08-07 23:31:18 +0200

    fixed home error

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -21,7 +21,6 @@ export type RootStackParamList = {
   Splash: undefined;
   Welcome: undefined;
   MainTabs: undefined;
-  Home: undefined;
   CouponDetail: { couponId: string };
   MyCoupons: undefined;
   Wallet: undefined;
